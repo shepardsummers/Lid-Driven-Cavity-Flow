acecec037f7887b8797730938f97cbf98b8a5bb6
Shepard Summers <115957978+shepardsummers@users.noreply.github.com>
2025-05-10 13:21:37 -0400

    Linearized Interior Nodes

--- eqm_d2q9.m
+++ eqm_d2q9.m
@@ -1,11 +1,8 @@
 function [f_eq] = eqm_d2q9(rho, u, ksi, w)
 
     guh = pagemtimes(ksi',u);
-    size(guh)
 
     udot = sum(u.^2, 1);
-
-    size(udot)
 
     f_eq = rho .* (1 + guh/(1/3) + (guh.^2)/(2/9) - udot/(2/3)) .* w';
     
